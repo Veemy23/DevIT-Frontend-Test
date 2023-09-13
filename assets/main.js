a520bdff82501a2e14c197d0312ae6065e7d8182
Nazar Danilov <veemy23@gmail.com>
2023-09-13 22:16:52 +0300

    Extra code is commented out

--- assets/main.js
+++ assets/main.js
@@ -302,30 +302,30 @@ copyButton.forEach(btn => {
 // 	[f3, [3, 4]],
 //  ]).then(console.log);
 
-function NotificationException() { }
-function ErrorException() { }
-function primitiveMultiply(a, b) {
-	const rand = Math.random();
-	if (rand < 0.5) {
-		return a * b;
-	} else if (rand > 0.85) {
-		throw new ErrorException()
-	} else {
-		throw new NotificationException()
-	}
-}
+// function NotificationException() { }
+// function ErrorException() { }
+// function primitiveMultiply(a, b) {
+// 	const rand = Math.random();
+// 	if (rand < 0.5) {
+// 		return a * b;
+// 	} else if (rand > 0.85) {
+// 		throw new ErrorException()
+// 	} else {
+// 		throw new NotificationException()
+// 	}
+// }
 
-function reliableMultiply(a, b) {
-	try {
-		return primitiveMultiply(a, b)
-	}
-	catch (e) {
-		if (e instanceof NotificationException) {
-			return reliableMultiply(a, b)
-		} else if (e instanceof ErrorException) {
-			return
-		}
-	}
-}
+// function reliableMultiply(a, b) {
+// 	try {
+// 		return primitiveMultiply(a, b)
+// 	}
+// 	catch (e) {
+// 		if (e instanceof NotificationException) {
+// 			return reliableMultiply(a, b)
+// 		} else if (e instanceof ErrorException) {
+// 			return
+// 		}
+// 	}
+// }
 
-console.log(reliableMultiply(8, 8));
+// console.log(reliableMultiply(8, 8));
